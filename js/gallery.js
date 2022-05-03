06358b75ccb8e56e17cccd96246a10020b6340fd
choiseungchul <tmdcjf152@naver.com>
2022-05-03 19:10:04 +0900

    메인/갤러리 수정

--- js/gallery.js
+++ js/gallery.js
@@ -4,18 +4,17 @@ const loading = document.querySelector(".loading");
 const input = document.querySelector("#search"); 
 const btnSearch = document.querySelector(".btnSearch"); 
 const base = "https://www.flickr.com/services/rest/?";
-const method1 = "flickr.interestingness.getList";
+const method1 = "flickr.favorites.getList";
 const method2 = "flickr.photos.search";
 const key = "e53d5102469cae4f59fcfc75e4acfc8e";
+const userId = "195454515@N07";
 const per_page = 20; 
 
-const url = `${base}method=${method1}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1`;
-
+const url = `${base}method=${method1}&api_key=${key}&user_id=${userId}&per_page=${per_page}&format=json&nojsoncallback=1`;
 
 callData(url);
 
 btnSearch.addEventListener("click", e=>{
-
     let tag = input.value; 
     tag = tag.trim();
     
@@ -23,8 +22,8 @@ btnSearch.addEventListener("click", e=>{
 
     if(tag !=""){
         callData(url); 
-    }else{
-        console.log("검색어를 입력하세요"); 
+    }else {
+        console.log("Please enter a search term"); 
         frame.innerHTML =""; 
         frame.classList.remove("on");
         frame.style.height = "auto"; 
@@ -33,9 +32,8 @@ btnSearch.addEventListener("click", e=>{
         if(errMsgs.length >0) frame.parentElement.querySelector("p").remove(); 
 
         const errMsg = document.createElement("p"); 
-        errMsg.append("검색어를 입력하세요"); 
+        errMsg.append("Please enter a search term."); 
         frame.parentElement.append(errMsg);
-
     }
 })
 
@@ -49,7 +47,7 @@ input.addEventListener("keyup", e=>{
         if(tag !=""){
             callData(url); 
         }else{
-            console.log("검색어를 입력하세요"); 
+            console.log("Please enter a search term"); 
             frame.innerHTML =""; 
             frame.classList.remove("on");
             frame.style.height = "auto"; 
@@ -58,7 +56,7 @@ input.addEventListener("keyup", e=>{
             if(errMsgs.length >0) frame.parentElement.querySelector("p").remove(); 
     
             const errMsg = document.createElement("p"); 
-            errMsg.append("검색어를 입력하세요"); 
+            errMsg.append("Please enter a search term."); 
             frame.parentElement.append(errMsg);
         }
     }
@@ -68,9 +66,7 @@ frame.addEventListener("click", e=>{
     e.preventDefault(); 
 
     let target = e.target.closest(".item").querySelector(".thumb"); 
-   
-  
-     
+
     if(e.target == target){ 
         let imgSrc = target.parentElement.getAttribute("href"); 
         
@@ -80,7 +76,7 @@ frame.addEventListener("click", e=>{
                     <div class="con">
                         <img src="${imgSrc}">
                     </div>
-                    <span class="close">CLOSE</span>
+                    <span class="close"><i class="fas fa-times close"></i></span>
                     `;
         pop.innerHTML = pops; 
         body.append(pop);  
@@ -89,11 +85,10 @@ frame.addEventListener("click", e=>{
 })
 
 body.addEventListener("click", e=>{
-
     let pop = body.querySelector(".pop"); 
 
     if(pop != null){
-        let close = pop.querySelector(".close"); 
+        let close = pop.querySelector("i"); 
 
         if(e.target == close){
             pop.remove(); 
@@ -101,7 +96,6 @@ body.addEventListener("click", e=>{
         }
     }
 })
-
 
 
 function callData(url){
@@ -122,8 +116,7 @@ function callData(url){
 
             createList(items);
             delayLoading();
-        }else{
-            
+        }else{            
             loading.classList.add("off"); 
 
             const errMsgs = frame.parentElement.querySelectorAll("p"); 
@@ -135,41 +128,32 @@ function callData(url){
 
             frame.classList.remove("on"); 
             frame.style.height = "auto"; 
-        }
-        
+        }        
     }); 
 }
 
 function createList(items){
     let htmls =""; 
     
-        items.map(data=>{
-            console.log(data); 
-    
-            let imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
-    
-            let imgSrcBig =`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
-    
-            htmls += `
-                    <li class="item">
-                        <div>
-                            <a href=${imgSrcBig}>
-                                <img src=${imgSrc} class="thumb" >
-                            </a>
-                            <p>${data.title}</p>
-                            <span>
-                                <img class="profile" src="http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg">
-                                <strong>${data.owener}</strong>
-                            </span>
-                        </div>
-                    </li>
-                    `;
-        })
-    
-        frame.innerHTML = htmls; 
+    items.map(data=>{
+        let imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+        let imgSrcBig =`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
+
+        htmls += `
+                <li class="item">
+                    <div>
+                        <a href=${imgSrcBig}>
+                            <img src=${imgSrc} class="thumb" >
+                        </a>
+                        <p>${data.title}</p>
+                    </div>
+                </li>
+                `;
+    })
+    frame.innerHTML = htmls; 
 }; 
 
-function delayLoading(){
+function delayLoading() {
     const imgs = frame.querySelectorAll("img"); 
     const len = imgs.length; 
     let count = 0; 
@@ -177,8 +161,7 @@ function delayLoading(){
     for(let el of imgs){
 
         el.onload =()=>{
-            count++;
-            
+            count++;            
             if(count == len) isoLayout(); 
         }
 
@@ -186,21 +169,10 @@ function delayLoading(){
         thumb.onerror = e =>{
             e.currentTarget.closest(".item").querySelector(".thumb").setAttribute("src", "img/k1.jpg"); 
         }
-
-        let profile = el.closest(".item").querySelector(".profile"); 
-        profile.onerror = e =>{
-            e.currentTarget.closest(".item").querySelector(".profile").setAttribute("src", "https://www.flickr.com/images/buddyicon.gif");
-        }
-
-        
     }
 }
 
-
-
-
-function isoLayout(){
-
+function isoLayout() {
     loading.classList.add("off"); 
     frame.classList.add("on"); 
 
